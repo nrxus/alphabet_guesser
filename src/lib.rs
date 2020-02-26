daaf9c16ab0519b50bb2b4b346b5b28e7ccda663
Andres <andresnrx@gmail.com>
2020-02-25 16:33:18 -0800

    handles past first character

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,67 @@
+use std::collections::{hash_map, HashMap, HashSet};
+
 pub fn get_alphabet(words: &[&str]) -> Vec<char> {
-    words.iter().flat_map(|w| w.chars().next()).collect()
+    let mut unprocessed_chars: HashSet<_> = words.iter().flat_map(|w| w.chars()).collect();
+
+    // pre-allocate
+    let mut before_chars: HashMap<char, _> = HashMap::new();
+    let mut after_chars: HashMap<char, _> = HashMap::new();
+
+    words
+        .windows(2)
+        .flat_map(|pair| {
+            let left = pair[0];
+            let right = pair[1];
+
+            left.chars()
+                .zip(right.chars())
+                .find(|(left, right)| left != right)
+        })
+        .for_each(|(left, right)| {
+            before_chars
+                .entry(left)
+                // we could pre-allocate this set with a capacity
+                .or_insert(HashSet::new())
+                .insert(right);
+            after_chars
+                .entry(right)
+                // we could pre-allocate this set with a capacity
+                .or_insert(HashSet::new())
+                .insert(left);
+        });
+
+    // pre-allocate
+    let mut alphabet = vec![];
+
+    while !unprocessed_chars.is_empty() {
+        if unprocessed_chars.len() == 1 {
+            alphabet.push(unprocessed_chars.into_iter().next().unwrap());
+            break;
+        }
+
+        let next_lowest = *unprocessed_chars
+            .iter()
+            .find(|c| !after_chars.contains_key(c))
+            .expect("ambiguous input");
+
+        before_chars
+            .remove(&next_lowest)
+            .unwrap_or_default()
+            .into_iter()
+            .for_each(|c| {
+                if let hash_map::Entry::Occupied(mut entry) = after_chars.entry(c) {
+                    entry.get_mut().remove(&next_lowest);
+                    if entry.get().is_empty() {
+                        entry.remove();
+                    }
+                }
+            });
+
+        unprocessed_chars.remove(&next_lowest);
+        alphabet.push(next_lowest);
+    }
+
+    alphabet
 }
 
 #[cfg(test)]
@@ -7,7 +69,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn empty() {
+    fn no_words() {
         assert_eq!(get_alphabet(&[]), vec![]);
     }
 
@@ -20,7 +82,12 @@ mod tests {
     }
 
     #[test]
-    fn handles_empty() {
+    fn empty_word() {
         assert_eq!(get_alphabet(&[""]), vec![]);
+    }
+
+    #[test]
+    fn ties_in_first_character() {
+        assert_eq!(get_alphabet(&["bac", "aaa", "acb"]), vec!['b', 'a', 'c']);
     }
 }
